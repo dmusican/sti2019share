fc7ba0c7d86c3d9632447563db8108cbdf9f673d
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 15:03:57 -0500

    w/day01morning/WrapperExample.java:11: [31m-      /*Wrapper w1 = new Wrapper();[m w/day01morning/WrapperExample.java:11: [32m+[m[32m      Wrapper w1 = new Wrapper();[m w/day01morning/WrapperExample.java:16: [31m-      System.out.println(w2.value);[m w/day01morning/WrapperExample.java:16: [32m+[m[32m      /*System.out.println(w2.value);[m

--- day01morning/WrapperExample.java
+++ day01morning/WrapperExample.java
@@ -8,12 +8,12 @@ public class WrapperExample {
       System.out.println(x);
       System.out.println(y);
 
-      /*Wrapper w1 = new Wrapper();
+      Wrapper w1 = new Wrapper();
       Wrapper w2 = new Wrapper();
       w1.value = 42;
       w2 = w1;
       System.out.println(w1.value);
-      System.out.println(w2.value);
+      /*System.out.println(w2.value);
       w2.value = 50;
       System.out.println(w1.value);
       System.out.println(w2.value);*/

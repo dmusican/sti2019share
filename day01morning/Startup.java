042700cc87bb867d14407f343d56f1614fb66225
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 11:14:37 -0500

    day01morning/Startup.java | 8 +++++++-

--- day01morning/Startup.java
+++ day01morning/Startup.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Startup {
   public static void main(String[] args) {
     
     System.out.println("Hello, world, I will convert temps for you.");
-    int tempF = 69;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("What Fahrenheit temp should I convert? ");
+    int tempF = scanner.nextInt();
+    
+    //int tempF = 69;
     double result = (tempF - 32) * 5./9;
     System.out.println("Answer = " + result);
   }

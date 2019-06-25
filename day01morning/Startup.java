c1a7fa766a72148a5cc35ca31ebf0ff40ac760d5
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 11:04:38 -0500

    w/day01morning/Startup.java:5: [31m-    int tempF = 68;[m w/day01morning/Startup.java:5: [32m+[m[32m    int tempF = 69;[m

--- day01morning/Startup.java
+++ day01morning/Startup.java
@@ -2,7 +2,7 @@ public class Startup {
   public static void main(String[] args) {
     
     System.out.println("Hello, world, I will convert temps for you.");
-    int tempF = 68;
+    int tempF = 69;
     double result = (tempF - 32) * 5./9;
     System.out.println("Answer = " + result);
   }

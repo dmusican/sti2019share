d8187ca16f0abc5d502059d271180dd4a4920e92
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 11:04:07 -0500

    w/day01morning/Startup.java:4: [31m-    System.out.println("Hello, world");[m w/day01morning/Startup.java:4: [32m+[m[32m    System.out.println("Hello, world, I will convert temps for you.");[m w/day01morning/Startup.java:5: [32m+[m[32m    int tempF = 68;[m w/day01morning/Startup.java:6: [32m+[m[32m    double result = (tempF - 32) * 5/9;[m w/day01morning/Startup.java:7: [32m+[m[32m    System.out.println("Answer = " + result);[m

--- day01morning/Startup.java
+++ day01morning/Startup.java
@@ -1,6 +1,9 @@
 public class Startup {
   public static void main(String[] args) {
     
-    System.out.println("Hello, world");
+    System.out.println("Hello, world, I will convert temps for you.");
+    int tempF = 68;
+    double result = (tempF - 32) * 5/9;
+    System.out.println("Answer = " + result);
   }
 }

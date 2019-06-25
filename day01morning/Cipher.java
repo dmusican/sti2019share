2aba92f6811321fd2b016465674e6b3de18987d2
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 14:09:40 -0500

    day01morning/Cipher.java | 9 +++++++--

--- day01morning/Cipher.java
+++ day01morning/Cipher.java
@@ -7,9 +7,14 @@ public class Cipher
     System.out.print("What is the text to be encrypted? ");
     Scanner ourKeyboard = new Scanner(System.in);
     String original = ourKeyboard.next();
-    System.out.println(original);
     System.out.println("What is the key? ");
     int key = ourKeyboard.nextInt();
-    System.out.println(key);
+    
+    String result = "";
+    for (int i=0; i < original.length; i++)
+    {
+      System.out.println(original.charAt(i));
+    }
+    
   }
 }

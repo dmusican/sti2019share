c642c8d3aac686878978dec410e57bdbf29a1d26
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-25 14:52:45 -0500

    New files added: ?? day01morning/WrapperExample.java

---
+++ day01morning/WrapperExample.java
@@ -0,0 +1,18 @@
+public class WrapperExample {
+
+   public static void main (String[] args) {
+
+      int x = 3;
+      int y = x;
+      x = 7;
+      System.out.println(x + " " + y);
+
+      Wrapper w1 = new Wrapper();
+      Wrapper w2 = new Wrapper();
+      w1.value = 42;
+      w2 = w1;
+      System.out.println(w1.value + " " + w2.value);
+      w2.value = 50;
+      System.out.println(w1.value + " " + w2.value);
+   }
+}

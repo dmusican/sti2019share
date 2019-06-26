e52bcd36485a87fedec4c61b08fb3e19f5477c03
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-26 15:22:49 -0500

    day02morning/Primes2.java | 14 +++++++-------

--- day02morning/Primes2.java
+++ day02morning/Primes2.java
@@ -4,26 +4,26 @@ class Primes2 {
 
     String[] possiblePrime = new String[size];
 
-    for (int i=0; i < possiblePrime.length; i++)
-    {
-      System.out.println(i + " " + possiblePrime[i]);
-    }
-
     /*for (int i=0; i < possiblePrime.length; i++)
     {
-      possiblePrime[i] = true;
+      System.out.println(i + " " + possiblePrime[i]);
+    }*/
+
+    for (int i=0; i < possiblePrime.length; i++)
+    {
+      possiblePrime[i] = "true";
     }
 
     for (int i=2; i < possiblePrime.length; i++)
     {
-      if (possiblePrime[i] == true)
+      if (possiblePrime[i].equals("true"))
       {
         System.out.println(i);
         for (int j = i+i; j < possiblePrime.length; j = j + i)
         {
-          possiblePrime[j] = false;
+          possiblePrime[j] = "false";
         }
       }
-    }*/
+    }
   }
 }

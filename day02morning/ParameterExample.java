5292598c663637411de027612754793ecf8c5596
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-26 15:45:19 -0500

    New files added: ?? day02morning/ParameterExample.java

---
+++ day02morning/ParameterExample.java
@@ -0,0 +1,27 @@
+public class ParameterExample {
+
+   public static void method1(int b) {
+      b = 7;
+   }
+
+   public static void method2(Wrapper t) {
+      t.value = 6;
+      t = new Wrapper();
+      t.value = 7;
+   }
+
+   public static void main(String[] args) {
+      int z = 9;
+      System.out.println(z);
+      method1(z);
+      System.out.println(z);
+
+      System.out.println();
+
+      Wrapper w = new Wrapper();
+      w.value = 5;
+      System.out.println(w.value);
+      method2(w);
+      System.out.println(w.value);
+   }
+}

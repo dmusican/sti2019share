fd53b4d9e50621853096fb31bbb042e4571163a0
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-26 15:28:04 -0500

    New files added: ?? day02morning/Primes3.java

---
+++ day02morning/Primes3.java
@@ -0,0 +1,36 @@
+class Primes2 {
+  public static void main(String[] args) {
+    int size = 100;
+
+    String[] possiblePrime = new String[size];
+
+    /*for (int i=0; i < possiblePrime.length; i++)
+    {
+      System.out.println(i + " " + possiblePrime[i]);
+    }*/
+    
+    for (String s : possiblePrime)
+    {
+      System.out.println(s);
+    }
+    
+    /*
+
+    for (int i=0; i < possiblePrime.length; i++)
+    {
+      possiblePrime[i] = "true";
+    }
+
+    for (int i=2; i < possiblePrime.length; i++)
+    {
+      if (possiblePrime[i].equals("true"))
+      {
+        System.out.println(i);
+        for (int j = i+i; j < possiblePrime.length; j = j + i)
+        {
+          possiblePrime[j] = "false";
+        }
+      }
+    }*/
+  }
+}

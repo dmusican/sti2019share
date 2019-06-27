f103cd16fbe39fd333437482bd17e583991cd702
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-27 10:42:06 -0500

    w/day03/Circle.java:9: [31m-    public double perimeters() {[m w/day03/Circle.java:9: [32m+[m[32m    public double perimeter() {[m

--- day03/Circle.java
+++ day03/Circle.java
@@ -6,7 +6,7 @@ public class Circle extends Shape {
     public double area() {
         return Math.PI * radius * radius;
     }
-    public double perimeters() {
+    public double perimeter() {
         return 2 * Math.PI * radius;
     }
 }

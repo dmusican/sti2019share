62a5756d2192e1e3fd93b906a7147b91a1c151b1
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-27 10:38:55 -0500

    day03/Circle.java | 6 +++---

--- day03/Circle.java
+++ day03/Circle.java
@@ -6,7 +6,7 @@ public class Circle extends Shape {
     public double area() {
         return Math.PI * radius * radius;
     }
-    public double perimeter() {
-        return 2 * Math.PI * radius;
-    }
+    // public double perimeter() {
+    //     return 2 * Math.PI * radius;
+    // }
 }

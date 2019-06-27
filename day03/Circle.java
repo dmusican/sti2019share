94fbb55f9a4960d262727216df71d4630cf14887
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-27 10:40:53 -0500

    w/day03/Circle.java:9: [31m-    @Overrides[m w/day03/Circle.java:9: [32m+[m[32m    @Override[m

--- day03/Circle.java
+++ day03/Circle.java
@@ -6,7 +6,7 @@ public class Circle extends Shape {
     public double area() {
         return Math.PI * radius * radius;
     }
-    @Overrides
+    @Override
     public double perimeter() {
         return 2 * Math.PI * radius;
     }

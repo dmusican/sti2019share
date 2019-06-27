73f2a850dc58b86df138b06478900dd0e1f1718e
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-27 09:50:24 -0500

    day03/#SlowSorts.java# | 69 --------------------------------------------------  day03/SlowSorts.java   | 10 ++++----

--- day03/SlowSorts.java
+++ day03/SlowSorts.java
@@ -40,21 +40,21 @@ public class SlowSorts {
         }
     }
 
-    /*public static void insertionSortOptimized(int[] array)
+    public static void insertionSortOptimized(int[] array)
     {
-        for (int i = _______________; i < ________________________; i++)
+        for (int i = 1; i < array.length; i++)
         {
             int j = i;
-            int swapVal = array[_____________________];
+            int swapVal = array[i];
 
-            while (j > 0 && _______________________________)
+            while (j > 0 && array[j-1] > swapVal)
             {
                 array[j] = array[j-1];
                 j--;
             }
             array[j] = swapVal;
         }
-        }*/
+    }
 
     public static void main(String[] args)
     {

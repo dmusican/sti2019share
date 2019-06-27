de5369211827a9c79cbfefc58a632e7ebe78073e
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-27 10:24:29 -0500

    day03/#Circle.java# | 8 --------

--- day03/Circle.java
+++ day03/Circle.java
@@ -5,4 +5,8 @@ public class Circle {
     }
     public double area() {
         return Math.PI * radius * radius;
-        
+    }
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}

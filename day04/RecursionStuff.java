39b12a65ca121336721d2ee96505e488b8e3595a
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-28 09:17:28 -0500

    day04/RecursionStuff.java | 14 ++++++++++++++

--- day04/RecursionStuff.java
+++ day04/RecursionStuff.java
@@ -12,9 +12,23 @@ public class RecursionStuff
     }  
   }
   
+  public static String reverse(String phrase)
+  {
+    if (phrase.length() == 1)
+    {
+      return phrase;
+    }
+    else
+    {
+      return phrase.substring(phrase.length()-1) +
+             reverse(phrase.substring(0,phrase.length()-1));
+    }
+  }
+  
   public static void main(String[] args)
   {
     System.out.println(factorial(3));
+    System.out.println(reverse("towers of hanoi"));
   }
   
 }

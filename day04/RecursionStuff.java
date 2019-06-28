d8e857b232c15da278453f30e3491f39597d1231
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2019-06-28 09:21:22 -0500

    day04/RecursionStuff.java | 64 +++++++++++++++++++++++------------------------

--- day04/RecursionStuff.java
+++ day04/RecursionStuff.java
@@ -1,35 +1,35 @@
 public class RecursionStuff
 {
-  public static int factorial(int n)
-  {
-    if (n == 1)
-    {
-      return 1;
-    }
-    else
-    {
-      return n * factorial(n-1);
-    }  
-  }
-  
-  public static String reverse(String phrase)
-  {
-    if (phrase.length() == 1)
-    {
-      return phrase;
-    }
-    else
-    {
-      return phrase.substring(phrase.length()-1) +
-             reverse(phrase.substring(0,phrase.length()-1));
-    }
-  }
-  
-  public static void main(String[] args)
-  {
-    System.out.println(factorial(3));
-    System.out.println("friend");
-    System.out.println(reverse("towers of hanoi"));
-  }
-  
+   public static int factorial(int n)
+   {
+      if (n == 1)
+      {
+         return 1;
+      }
+      else
+      {
+         return n * factorial(n-1);
+      }  
+   }
+   
+   public static String reverse(String phrase)
+   {
+      if (phrase.length() == 1)
+      {
+         return phrase;
+      }
+      else
+      {
+         return phrase.substring(phrase.length()-1) +
+                 reverse(phrase.substring(0,phrase.length()-1));
+      }
+   }
+   
+   public static void main(String[] args)
+   {
+      System.out.println(factorial(3));
+      System.out.println("friend");
+      System.out.println(reverse("towers of hanoi"));
+   }
+   
 }
